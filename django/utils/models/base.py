aa50c7e27b4ddcf0e44463b246bd92d6a19f7e95
Vova Zakharov <vzakharov@gmail.com>
2025-04-10 21:53:07 +0200

    ditch uuid for now

--- django/utils/models/base.py
+++ django/utils/models/base.py
@@ -6,7 +6,6 @@ from django.utils import timezone
 import uuid
 
 class BaseModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
